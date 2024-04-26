28b67e43a090f00d65a28303faa4c831c3c1dcd8
Satish522 <satishsubudhi522@gmail.com>
2024-04-27 00:38:07 +0530

    Minor changes

--- src/org/satish/array/sorting/SelectionSort.java
+++ src/org/satish/array/sorting/SelectionSort.java
@@ -1,15 +1,19 @@
 package org.satish.array.sorting;
 
+import java.util.Random;
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int arr[] = {5,3,9,2,6,3,7};
+		//int arr[] = {5,3,9,2,6,3,7};
+		int arr[] = generateRandomeArray(10);
 		int size = arr.length;
 		int steps = 0;
 		System.out.println("Before Sorting...");
 		for(int i : arr) {
 			System.out.print(i + " ");
 		}
+		System.out.println();
 		
 		//Approach 1
 		int minIndex = -1;
@@ -31,12 +35,20 @@ public class SelectionSort {
 		
 		
 		
-		System.out.println("\nNumber of steps :"+steps);
+		System.out.println("\n\nNumber of steps :"+steps);
 		System.out.println("After Selection Sorting...");
 		for(int i : arr) {
 			System.out.print(i + " ");
 		}
 
+	}
+	
+	private static int[] generateRandomeArray(int size) {
+		int arr[] = new int[size];
+		for(int i=0; i<size-1;i++) {
+			arr[i] = new Random().nextInt(10);
+		}
+		return arr;
 	}
 
 }
