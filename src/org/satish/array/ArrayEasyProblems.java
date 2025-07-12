f97cb6b61fa29affda3c7551d08b038caa8c488e
Satish522 <satishsubudhi522@gmail.com>
2025-07-12 17:28:37 +0530

    ArrayEasy problems

--- src/org/satish/array/ArrayEasyProblems.java
+++ src/org/satish/array/ArrayEasyProblems.java
@@ -1,12 +1,14 @@
 package org.satish.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.satish.array.util.DSAUtil;
 
-public class LargestElementInArray {
+public class ArrayEasyProblems {
 
 	public static void main(String[] args) {
 		int arr[] = DSAUtil.generateRandomeArray(10);
@@ -18,6 +20,69 @@ public class LargestElementInArray {
 		removeDuplicates(sortedArr);
 		leftRotateByOnePlace(arr);
 		leftRotateByDPlace(arr, 4);
+		rightRotateByDPlace(arr, 5); // Finish by your self both approach ??
+		moveZeroToEndOfArray(arr);
+		
+		
+	}
+
+	private static void moveZeroToEndOfArray(int[] arr) {
+		
+	//	bruteForceMoveZeroToEndOfArray(arr);
+		
+		optimalMoveZeroToEndOfArray(arr);
+		
+	}
+
+	private static void optimalMoveZeroToEndOfArray(int[] arr) {
+		System.out.println("\nShifting 0 to end of array optmial");
+		int j = -1;
+		for(int i = 0; i<arr.length; i++) {
+			if(arr[i] == 0) {
+				j=i;
+				break;
+			}
+		}
+		
+		for(int i = j+1; i < arr.length; i++) {
+			if(arr[i] != 0) {
+				int tmp = arr[i];
+	            arr[i] = arr[j];
+	            arr[j] = tmp;
+				j++;
+			}
+		}
+		System.out.print(" >> ");
+		Arrays.stream(arr).forEach(ele -> System.out.print(ele+" "));
+	}
+
+	private static void swap(int i, int j) {
+		
+		
+	}
+
+	private static void bruteForceMoveZeroToEndOfArray(int[] arr) {
+		System.out.println("\nShifting 0 to end of array");
+		List temp = new ArrayList();
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] != 0)
+				temp.add(arr[i]);
+		}
+		
+		for(int i =0 ; i< temp.size(); i++) {
+			arr[i] = (int) temp.get(i);
+		}
+		
+		for(int i = temp.size(); i< arr.length; i++) {
+			arr[i] = 0;
+		}
+		System.out.print(" >> ");
+		Arrays.stream(arr).forEach(ele -> System.out.print(ele+" "));
+		
+	}
+
+	private static void rightRotateByDPlace(int[] arr, int i) {
+		
 		
 	}
 
@@ -28,14 +93,38 @@ public class LargestElementInArray {
 			 d = d % arr.length;
 		}
 		System.out.println("Actual rotation "+ d);
-		bruteForceleftRotateByDPlace(arr, d);
+		//bruteForceLeftRotateByDPlace(arr, d);
 		
-		
+		//no better solution so jumping to optmial
+		optimalLeftRotateByDPlace(arr, d);
 		
 		
 	}
 
-	private static void bruteForceleftRotateByDPlace(int[] arr, int d) {
+	private static void optimalLeftRotateByDPlace(int[] arr, int d) {
+		int n = arr.length;
+		System.out.println("\nRotate left by "+d+" place optmial");
+		reverse(arr, 0, d-1);
+		reverse(arr, d, n-1);
+		reverse(arr, 0, n-1);
+		System.out.print(" >> ");
+		Arrays.stream(arr).forEach(ele -> System.out.print(ele+" "));
+	}
+	
+	
+
+	private static void reverse(int[] arr, int start, int end) {
+		while(start <= end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+		
+	}
+
+	private static void bruteForceLeftRotateByDPlace(int[] arr, int d) {
 		//bruteForce approach
 		System.out.println("\nRotate left by "+d+" place");
 		Arrays.stream(arr).forEach(ele -> System.out.print(ele+" "));
